0694c76a7fd60d16c083958ac1a7b419b223b11e
Jared Thomas <jjthom87@yahoo.com>
2017-10-28 00:24:40 -0400

    alternative answer to exercise

--- pig_latin_translator.py
+++ pig_latin_translator.py
@@ -5,14 +5,24 @@ words = original.split()
 new_words = []
 
 for word in words:
-	if(word[0] in "aeiou"):
+	if word[0] in "aeiou":
 		new_words.append(word + "yay")
 	else:
-		split = list(word)
-		first_letter = split[0]
-		split.remove(split[0])
-		joined = "".join(split)
-		joined = joined + first_letter + "ay"
-		new_words.append(joined)
+		#split = list(word)
+		#first_letter = split[0]
+		#split.remove(split[0])
+		#joined = "".join(split)
+		#joined = joined + first_letter + "ay"
+		#new_words.append(joined)
+		vowel_pos = 0
+		for letter in word:
+			if letter not in "aeiou":
+				vowel_pos = vowel_pos + 1
+			else:
+				break
+			cons = word[:vowel_pos]
+			the_rest = word[vowel_pos:]
+			new_word = the_rest + cons + "ay"
+			new_words.append(new_word)
 
 print(" ".join(new_words))
